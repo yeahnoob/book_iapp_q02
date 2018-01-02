d9e17a9edf6b5ae9e369f2fe3e01f4b8f4dcada7
Xanadu Zhang <yeahnoob@buaa.edu.cn>
2018-01-02 11:28:12 +0800

    code format

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,21 @@ fn main() {
         for j in &op {
             for k in &op {
                 for l in &op {
-                    let expr = format!("{}{}{}{}{}{}{}", &c[3..], j, &c[2..3], k, &c[1..2], l, &c[0..1]);
+                    let expr = format!(
+                        "{}{}{}{}{}{}{}",
+                        &c[3..],
+                        j,
+                        &c[2..3],
+                        k,
+                        &c[1..2],
+                        l,
+                        &c[0..1]
+                    );
                     if expr.len() > 4 {
                         let mut pr = Interpreter::from(&expr);
                         let result = pr.interpret().unwrap().round() as i64;
                         if i == result {
-                            println!( "{}", expr);
+                            println!("{}", expr);
                         }
                     }
                 }
